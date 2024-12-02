4ef3aae721aeaa40e6e06e023af7efc4d6cddef9
Mohammad Bablu YT (ALI AKBAR) <91384223+mohammadbabluyt@users.noreply.github.com>
2024-12-02 10:31:35 +0600

    Add files via upload

---
+++ Week 1/EscapeSequence.java
@@ -0,0 +1,22 @@
+/*
+
+\n
+\t
+\"
+\'
+
+*/
+
+public class EscapeSequence {
+	public static void main(String[] args) {
+		
+		System.out.println("Mohammad Bablu YT \n2024"); // \n diye new line create kore.
+		
+		System.out.println("A B C \t D"); // \t diye tab create kore.
+		
+		System.out.println(" \"Bablu\" "); // \" double Quote create kore.
+		
+		System.out.println("Good Morning \'Bablu\'"); // \' single quote create kore.
+		
+	}
+}
