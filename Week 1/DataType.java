098c1e3fae21ba807eb9ecde331ae9c5b8c0db88
Mohammad Bablu YT (ALI AKBAR) <91384223+mohammadbabluyt@users.noreply.github.com>
2024-12-03 18:10:33 +0600

    Add files via upload

---
+++ Week 1/DataType.java
@@ -0,0 +1,25 @@
+public class DataType {
+	public static void main(String[] args) {
+		
+		boolean b = true ; // daynamic
+		// b = false; //same aas above
+		
+		int i = 100;
+		
+		double d = 10.5;
+		
+		float f = 10;
+		
+		char c = 'A';
+		
+		short s = 12345;
+	
+	  System.out.println("b = "+b);
+	  System.out.println("i = "+i);
+	  System.out.println("d = "+d);
+	  System.out.println("f = "+f);
+	  System.out.println("c = "+c);
+	  System.out.println("s = "+s);
+		
+	}
+}
