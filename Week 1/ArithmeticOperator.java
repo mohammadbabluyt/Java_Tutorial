72daa1e3772320c077bb3de251cb8a8ffb3b6e78
Mohammad Bablu YT (ALI AKBAR) <91384223+mohammadbabluyt@users.noreply.github.com>
2024-12-13 10:03:25 +0600

    Add files via upload

---
+++ Week 1/ArithmeticOperator.java
@@ -0,0 +1,25 @@
+public class ArithmeticOperator {
+	public static void main(String[] args) {
+		
+		int num1, num2, result;
+		
+		num1 = 35;
+		num2 = 8;
+		
+		result = num1 + num2;
+		System.out.println(result);
+		
+		result = num1 - num2;
+		System.out.println(result);
+		
+		result = num1 * num2;
+		System.out.println(result);
+		
+	    double result2 = (double) num1 / num2;
+		System.out.println(result2);
+		
+		result = num1 % num2;
+		System.out.println(result);
+		
+	}
+}
